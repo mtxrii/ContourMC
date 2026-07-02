f9d79a8e6fb87910fdc2ee26ddc96a70afe0ebe5
Enrique <edavalos@ucsc.edu>
2026-07-02 01:03:27 -0700

    Add PlayerQuitListener
    with custom quit message

---
+++ src/main/java/com/mtxrii/contourmc/listener/PlayerQuitListener.java
@@ -0,0 +1,22 @@
+package com.mtxrii.contourmc.listener;
+
+import com.mtxrii.contourmc.message.Message;
+import com.mtxrii.contourmc.message.MessagePrefix;
+import com.sxtanna.platform.archetype.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+@Component
+public class PlayerQuitListener implements Listener {
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        String playerName = player.getName();
+
+        Message quitMsg = new Message(MessagePrefix.GAME, "{} has left", playerName);
+        event.quitMessage(quitMsg.getMessageComponent());
+    }
+}
