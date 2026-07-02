2ad8353ffa6ec6bd4ffd5b0a509dbb5d8e4ab64c
Enrique <edavalos@ucsc.edu>
2026-07-02 01:00:59 -0700

    Add PlayerJoinListener
    with custom join message

---
+++ src/main/java/com/mtxrii/contourmc/listener/PlayerJoinListener.java
@@ -0,0 +1,22 @@
+package com.mtxrii.contourmc.listener;
+
+import com.mtxrii.contourmc.message.Message;
+import com.mtxrii.contourmc.message.MessagePrefix;
+import com.sxtanna.platform.archetype.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+@Component
+public class PlayerJoinListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String playerName = player.getName();
+
+        Message joinMsg = new Message(MessagePrefix.GAME, "{} has joined", playerName);
+        event.joinMessage(joinMsg.getMessageComponent());
+    }
+}
