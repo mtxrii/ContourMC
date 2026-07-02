a414dacbbc58018a2c023214c2ce5e907a28efc9
Enrique <edavalos@ucsc.edu>
2026-07-02 00:51:49 -0700

    Incept new death messages with detailed damage and projectile information
    Add comprehensive death message handling for non-PvP deaths including damage causes, entity attackers, and projectile sources with shooter attribution. Broadcast all death messages to server.

--- src/main/java/com/mtxrii/contourmc/listener/PlayerDeathListener.java
+++ src/main/java/com/mtxrii/contourmc/listener/PlayerDeathListener.java
@@ -5,11 +5,16 @@ import com.mtxrii.contourmc.message.Message;
 import com.mtxrii.contourmc.message.MessagePrefix;
 import com.mtxrii.contourmc.service.SpawnpointService;
 import com.sxtanna.platform.archetype.Component;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.projectiles.ProjectileSource;
 
 @Component
 public class PlayerDeathListener implements Listener {
@@ -30,9 +35,38 @@ public class PlayerDeathListener implements Listener {
         if (killer != null) {
             deathMessage = new Message(MessagePrefix.GAME, "{} was killed by {}", victim.getName(), killer.getName());
 
+        } else {
+            deathMessage = new Message(MessagePrefix.GAME, "{} has died", victim.getName());
+
+            EntityDamageEvent lastDamageCause = victim.getLastDamageCause();
+            if (lastDamageCause != null) {
+                EntityDamageEvent.DamageCause cause = lastDamageCause.getCause();
+                deathMessage = deathMessage.append(new Message(MessagePrefix.BLANK, " from {}", formatEnumName(cause)));
+
+                if (lastDamageCause instanceof EntityDamageByEntityEvent damageByEntity) {
+                    Entity damager = damageByEntity.getDamager();
+                    deathMessage = new Message(MessagePrefix.GAME, "{} was killed by {}", victim.getName(), formatEnumName(damager.getType()));
+
+                    if (damager instanceof Projectile projectile) {
+                        Message shooterLabel = null;
+                        ProjectileSource shooter = projectile.getShooter();
+                        if (shooter instanceof Player shooterPlayer) {
+                            shooterLabel = new Message(MessagePrefix.BLANK, " by {}", shooterPlayer.getName());
+                        } else if (shooter instanceof Entity shooterEntity) {
+                            shooterLabel = new Message(MessagePrefix.BLANK, " by {}", formatEnumName(shooterEntity.getType()));
+                        }
+                        deathMessage = new Message(MessagePrefix.GAME, "{} was shot", victim.getName());
+
+                        if (shooterLabel != null) {
+                            deathMessage = deathMessage.append(shooterLabel);
+                        }
+                    }
+                }
+            }
         }
 
         event.setShowDeathMessages(false);
+        deathMessage.sendToAll();
 
         this.plugin.getServer().getScheduler().runTask(this.plugin, () -> {
             victim.spigot().respawn();
