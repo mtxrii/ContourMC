0485bb17f73e58f4b44901dc8de0483b2dbb8663
Enrique <edavalos@ucsc.edu>
2026-07-17 00:44:36 -0700

    Teleport player to random spawnpoint on join
    Inject SpawnpointService into PlayerJoinListener and teleport players to random spawnpoint after sending join message.

--- src/main/java/com/mtxrii/contourmc/listener/PlayerJoinListener.java
+++ src/main/java/com/mtxrii/contourmc/listener/PlayerJoinListener.java
@@ -1,7 +1,9 @@
 package com.mtxrii.contourmc.listener;
 
+import com.google.inject.Inject;
 import com.mtxrii.contourmc.message.Message;
 import com.mtxrii.contourmc.message.MessagePrefix;
+import com.mtxrii.contourmc.service.SpawnpointService;
 import com.sxtanna.platform.archetype.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,8 +12,15 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 /// On join:
 /// - Send custom join message
+/// - Teleport player to random spawnpoint
 @Component
 public class PlayerJoinListener implements Listener {
+    private SpawnpointService spawnpointService;
+
+    @Inject
+    public PlayerJoinListener(SpawnpointService spawnpointService) {
+        this.spawnpointService = spawnpointService;
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -20,5 +29,7 @@ public class PlayerJoinListener implements Listener {
 
         Message joinMsg = new Message(MessagePrefix.GAME, "{} has joined", playerName);
         event.joinMessage(joinMsg.getMessageComponent());
+
+        this.spawnpointService.teleportLivingEntityToRandomSpawnpoint(event.getPlayer());
     }
 }
