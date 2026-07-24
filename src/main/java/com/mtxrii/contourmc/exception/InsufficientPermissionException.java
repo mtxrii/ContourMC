d14e1a87ca879155644fdf4824b34309556ced5d
Enrique <edavalos@ucsc.edu>
2026-07-24 00:28:34 -0700

    Change InsufficientPermissionException to extend CommandArgumentException
    Modify InsufficientPermissionException to inherit from CommandArgumentException instead of RuntimeException. Update constructor to pass prefix to parent class and add `@Override` annotation to `getMessageComponent()` method.

--- src/main/java/com/mtxrii/contourmc/exception/InsufficientPermissionException.java
+++ src/main/java/com/mtxrii/contourmc/exception/InsufficientPermissionException.java
@@ -4,17 +4,18 @@ import com.mtxrii.contourmc.message.Message;
 import com.mtxrii.contourmc.message.MessagePrefix;
 import net.kyori.adventure.text.Component;
 
-public class InsufficientPermissionException extends RuntimeException {
+public class InsufficientPermissionException extends CommandArgumentException {
     private static final String DEFAULT_MESSAGE = "You do not have permission to perform this action.";
 
     private final Message message;
 
     public InsufficientPermissionException(MessagePrefix prefix) {
         final Message errMsg = new Message(prefix, true, DEFAULT_MESSAGE);
-        super(errMsg.getMessage());
+        super(prefix, errMsg.getMessage());
         this.message = errMsg;
     }
 
+    @Override
     public Component getMessageComponent() {
         return this.message.getMessageComponent();
     }
