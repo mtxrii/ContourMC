36d26b846d8d0ab8e8e6258870b649abb0bfc165
Enrique <edavalos@ucsc.edu>
2026-07-28 22:55:45 -0700

    Add dependency injection to PlayerSignClickListener for kit functionality
    Inject Plugin and KitService dependencies via constructor to enable kit equipping when players interact with kit signs.

--- src/main/java/com/mtxrii/contourmc/listener/PlayerSignClickListener.java
+++ src/main/java/com/mtxrii/contourmc/listener/PlayerSignClickListener.java
@@ -1,11 +1,22 @@
 package com.mtxrii.contourmc.listener;
 
+import com.google.inject.Inject;
+import com.mtxrii.contourmc.service.KitService;
 import com.sxtanna.platform.archetype.Component;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 
 /// On player right-click sign block:
 /// - If text matches kit sign:
 ///   - Equip kit
 @Component
 public class PlayerSignClickListener implements Listener {
+    private Plugin plugin;
+    private KitService kitService;
+
+    @Inject
+    public PlayerSignClickListener(Plugin plugin, KitService kitService) {
+        this.plugin = plugin;
+        this.kitService = kitService;
+    }
 }
