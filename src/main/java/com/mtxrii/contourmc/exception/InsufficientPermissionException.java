0cba5f49657fcc813ea5041f9b045884d7f9189f
Enrique <edavalos@ucsc.edu>
2026-07-30 00:33:59 -0700

    Remove unused message field and getMessageComponent override from InsufficientPermissionException
    Delete redundant message field and override method as the exception message is already passed to the parent constructor and handled there.

--- src/main/java/com/mtxrii/contourmc/exception/InsufficientPermissionException.java
+++ src/main/java/com/mtxrii/contourmc/exception/InsufficientPermissionException.java
@@ -2,21 +2,12 @@ package com.mtxrii.contourmc.exception;
 
 import com.mtxrii.contourmc.message.Message;
 import com.mtxrii.contourmc.message.MessagePrefix;
-import net.kyori.adventure.text.Component;
 
 public class InsufficientPermissionException extends CommandArgumentException {
     private static final String DEFAULT_MESSAGE = "You do not have permission to perform this action.";
 
-    private final Message message;
-
     public InsufficientPermissionException(MessagePrefix prefix) {
         final Message errMsg = new Message(prefix, true, DEFAULT_MESSAGE);
         super(prefix, errMsg.getMessage());
-        this.message = errMsg;
-    }
-
-    @Override
-    public Component getMessageComponent() {
-        return this.message.getMessageComponent();
     }
 }
