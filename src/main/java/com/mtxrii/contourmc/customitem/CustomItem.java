31e6743ff06c59aad4056b6bba5e31ac3af47041
Enrique <edavalos@ucsc.edu>
2026-07-30 02:03:12 -0700

    Replace getEffect() method with Lombok @Getter annotation in CustomItem
    Remove manual getter implementation for effect field and use Lombok @Getter annotation to reduce boilerplate code.

--- src/main/java/com/mtxrii/contourmc/customitem/CustomItem.java
+++ src/main/java/com/mtxrii/contourmc/customitem/CustomItem.java
@@ -4,6 +4,7 @@ import com.mtxrii.contourmc.message.Message;
 import com.mtxrii.contourmc.message.MessagePrefix;
 import com.mtxrii.contourmc.util.ItemUtil;
 import com.mtxrii.contourmc.util.TextUtil;
+import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -14,16 +15,12 @@ public enum CustomItem {
 
     private final Material material;
     private final String itemDisplayName;
-    private final CustomItemEffect effect;
+    @Getter private final CustomItemEffect effect;
 
     CustomItem(Material material, String itemDisplayName, CustomItemEffect effect) {
         this.material = material;
         this.itemDisplayName = itemDisplayName;
         this.effect = effect;
-    }
-
-    public CustomItemEffect getEffect() {
-        return this.effect;
     }
 
     public static CustomItem getCustomItem(Material material, ItemStack itemHeld) {
