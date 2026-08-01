2633217d8e362bf452953f7168067ede755f9a84
Enrique <edavalos@ucsc.edu>
2026-08-01 00:40:48 -0700

    Add LASER_POINTER custom item with damage and particle effects
    Implement LASER_POINTER custom item using BLAZE_ROD that shoots a 128-block raycast laser with FLAME and CLOUD particles. Deals 2.0 damage to first entity hit (excluding shooter), sends hit confirmation message to player, and stops on block collision.

--- src/main/java/com/mtxrii/contourmc/customitem/CustomItem.java
+++ src/main/java/com/mtxrii/contourmc/customitem/CustomItem.java
@@ -2,17 +2,26 @@ package com.mtxrii.contourmc.customitem;
 
 import com.mtxrii.contourmc.message.Message;
 import com.mtxrii.contourmc.message.MessagePrefix;
+import com.mtxrii.contourmc.particle.ParticleSpawn;
 import com.mtxrii.contourmc.util.ItemUtil;
 import com.mtxrii.contourmc.util.TextUtil;
 import lombok.Getter;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import java.util.Collection;
 
 public enum CustomItem {
     LIGHTNING_STICK(Material.BREEZE_ROD, "Smite Stick", 5, (targetLocation, ignoredUser) -> {
         targetLocation.getWorld().strikeLightningEffect(targetLocation);
     }),
+
     TNT_PLANTER_SEED(Material.PUMPKIN_SEEDS, "TNT Seeds", 10, (targetLocation, ignoredUser) -> {
         targetLocation.getWorld().spawn(
                 targetLocation,
@@ -23,6 +32,51 @@ public enum CustomItem {
                 }
         );
     }),
+
+    LASER_POINTER(Material.BLAZE_ROD, "Laser Pointer", (ignoreTargetLocation, player) -> {
+        final int LASER_DISTANCE = 128;
+        final double LASER_DAMAGE = 2.0;
+        Location headLocation = player.getEyeLocation();
+        Vector lookDirection = headLocation.getDirection().multiply(0.125);
+        laserGenerateLoop:
+        for (double i = 0; i < LASER_DISTANCE; i++) {
+            headLocation.add(lookDirection);
+            new ParticleSpawn(Particle.FLAME, player.getWorld(), headLocation).spawn();
+            new ParticleSpawn(Particle.CLOUD, player.getWorld(), headLocation).spawn();
+
+            Material blockTypeAtLaser = headLocation.getBlock().getType();
+            if (!blockTypeAtLaser.isAir()) {
+                break;
+            }
+
+            boolean hitPlayer = false;
+            Collection<LivingEntity> entitiesAtLaser = headLocation.getNearbyLivingEntities(1);
+            for (LivingEntity entity : entitiesAtLaser) {
+                if (entity instanceof Player targetPlayer) {
+                    if (targetPlayer.getName().equals(player.getName())) {
+                        continue;
+                    }
+                }
+                entity.damage(LASER_DAMAGE, player);
+                String entityName;
+                if (entity instanceof Player targetPlayer) {
+                    entityName = targetPlayer.getName();
+                    hitPlayer = true;
+                } else {
+                    entityName = entity.getName();
+                }
+                String entityHitName = hitPlayer ? "player" : "entity";
+                new Message(
+                        MessagePrefix.GAME,
+                        "Hit {}: {}",
+                        entityHitName,
+                        entityName
+                ).sendTo(player);
+                break laserGenerateLoop;
+            }
+        }
+    }),
+
     DEBUG_HOE(Material.COPPER_HOE, "Debug Hoe", (targetLocation, ignoredUser) -> {
         new Message(MessagePrefix.GAME, "Pointing at {}", TextUtil.formatLocation(targetLocation)).sendToAll();
     });
