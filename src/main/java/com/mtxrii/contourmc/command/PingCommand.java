f22d1913db63f4b1d77e188a480d737e8c0b9224
Enrique <edavalos@ucsc.edu>
2026-08-15 23:34:51 -0700

    Add command description to ping command
    Add description "Gets your ping in ms" to @CommandDescription annotation for better command documentation.

--- src/main/java/com/mtxrii/contourmc/command/PingCommand.java
+++ src/main/java/com/mtxrii/contourmc/command/PingCommand.java
@@ -17,7 +17,7 @@ public class PingCommand {
     @Inject private RankService rankService;
 
     @Command("ping")
-    @CommandDescription("")
+    @CommandDescription("Gets your ping in ms")
     public void ping(
             @NotNull final Source sender
     ) {
