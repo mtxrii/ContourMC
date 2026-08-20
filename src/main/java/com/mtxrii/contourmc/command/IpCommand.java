92777c2edc5dd701a8fef01bd3610dddf00d438f
Enrique <edavalos@ucsc.edu>
2026-08-20 00:46:09 -0700

    Add ipaddress command to retrieve player IP addresses for moderators
    Implement new `/ipaddress <player>` command requiring MEDIATOR rank that displays a player's IP address using the PlayerRegistryService.

---
+++ src/main/java/com/mtxrii/contourmc/command/IpCommand.java
@@ -0,0 +1,49 @@
+package com.mtxrii.contourmc.command;
+
+import com.google.inject.Inject;
+import com.mtxrii.contourmc.Rank;
+import com.mtxrii.contourmc.message.Message;
+import com.mtxrii.contourmc.message.MessagePrefix;
+import com.mtxrii.contourmc.service.PlayerRegistryService;
+import com.mtxrii.contourmc.service.RankService;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.incendo.cloud.annotations.Argument;
+import org.incendo.cloud.annotations.Command;
+import org.incendo.cloud.annotations.CommandDescription;
+import org.incendo.cloud.annotations.processing.CommandContainer;
+import org.incendo.cloud.paper.util.sender.Source;
+import org.jetbrains.annotations.NotNull;
+
+@CommandContainer
+public class IpCommand {
+    @Inject private PlayerRegistryService playerRegistryService;
+    @Inject private RankService rankService;
+
+    @Command("ipaddress <player>")
+    @CommandDescription("Get a player's IP address")
+    public void ping(
+            @NotNull final Source sender,
+            @Argument("player") final String targetPlayer
+    ) {
+        if (sender.source() instanceof Player player) {
+            this.rankService.requireRank(MessagePrefix.ENV, Rank.MEDIATOR, player);
+        }
+
+        Player target = Bukkit.getPlayer(targetPlayer);
+        if (target == null) {
+            new Message(
+                    MessagePrefix.MOD,
+                    true,
+                    "Player not found"
+            ).sendTo(sender);
+        } else {
+            new Message(
+                    MessagePrefix.MOD,
+                    "{}'s IP address is {}",
+                    target.getName(),
+                    this.playerRegistryService.getPlayerIp(target.getUniqueId())
+            ).sendTo(sender);
+        }
+    }
+}
